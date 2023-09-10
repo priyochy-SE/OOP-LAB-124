72bfea08143040f2d4d521e6db4172adf7690287
PRIYABRATA CHOWDHURY <142817175+priyochy-SE@users.noreply.github.com>
2023-09-11 00:07:34 +0600

    Task - 02

--- Laboratories Report - 03/Triangle_Final.java
+++ Laboratories Report - 03/Triangle_Final.java
@@ -3,7 +3,7 @@
 public class Triangle_Final 
 {
      public static void main(String[] args) {
-         //base=4,height=6
+         
          Triangle triangle = new Triangle(8, 4);
                 
          System.out.println("Area of the Triangle: " + triangle.getArea());
